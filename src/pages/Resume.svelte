12d828292ecfe2f818d8a425db6b0d8eb182d1ec
Dmitry Mikhaylov <dimamix199@gmail.com>
2022-04-29 20:06:18 +0400

    Adjust contacts

--- src/pages/Resume.svelte
+++ src/pages/Resume.svelte
@@ -10,12 +10,12 @@
 
 <main class="bg-snow min-w-screen min-h-screen py-8 px-4">
   <div class="max-w-ideal mx-auto">
-    <Title class="mb-8"/>
-    <Contacts class="mb-8"/>
-    <About class="mb-8"/>
-    <Skills class="mb-8"/>
-    <Experience class="mb-8"/>
-    <Education class="mb-8"/>
+    <Title/>
+    <Contacts class="mt-8"/>
+    <About class="mt-8"/>
+    <Skills class="mt-8 print:break-after-page"/>
+    <Experience class="mt-8"/>
+    <Education class="mt-8"/>
     <!-- <Languages class="mb-8"/> -->
   </div>
 </main>
--- src/sections/Contacts.svelte
+++ src/sections/Contacts.svelte
@@ -1,57 +1,70 @@
 <script>
+	import LinkedInIcon from '../icons/LinkedInIcon.svelte';
+	import PhoneIcon from '../icons/PhoneIcon.svelte';
+	import EmailIcon from '../icons/EmailIcon.svelte';
+	import LocationIcon from '../icons/LocationIcon.svelte';
+	import LinkIcon from '../icons/LinkIcon.svelte';
   import Text from '../components/Text.svelte';
 </script>
 
 <section class={`w-full flex flex-wrap flex-col sm:flex-row print:flex-row justify-around items-center ${$$props.class ?? ''}`}>
 
-  <div class="border-solid border-onyx border-t 
-    w-full sm:basis-full print:basis-full 
-    sm:self-stretch print:self-stretch 
+  <div class="border-solid border-onyx border-t
+    w-full sm:basis-full print:basis-full
+    sm:self-stretch print:self-stretch
     mb-6"
   ></div>
 
-  <Text class="text-sm font-semibold">
+  <Text class="text-sm font-semibold flex items-center">
+    <LocationIcon class="mr-1"/>
     Tbilisi, Georgia
   </Text>
 
-  <div class="border-solid border-onyx 
-    border-t sm:border-t-0 print:border-t-0 
-    border-l-0 sm:border-l print:border-l 
-    w-24 sm:w-0 print:w-0 
-    sm:self-stretch print:self-stretch 
-    my-3 sm:my-0 print:my-0 mx-0 
+  <div class="border-solid border-onyx
+    border-t sm:border-t-0 print:border-t-0
+    border-l-0 sm:border-l print:border-l
+    w-24 sm:w-0 print:w-0
+    sm:self-stretch print:self-stretch
+    my-3 sm:my-0 print:my-0 mx-0
     sm:mx-2 print:mx-2"
   ></div>
 
   <Text class="text-sm font-semibold">
-    <a href="mailto:dmitry.mihaylov.main@gmail.com">dmitry.mihaylov.main@gmail.com</a>
+    <a href="mailto:dmitry.mihaylov.main@gmail.com"  class="flex items-center">
+      <EmailIcon class="mr-1"/>
+      dmitry.mihaylov.main@gmail.com
+    </a>
   </Text>
 
-  <div class="border-solid border-onyx 
-    border-t sm:border-t-0 print:border-t-0 
-    border-l-0 sm:border-l print:border-l 
-    w-24 sm:w-0 print:w-0 
-    sm:self-stretch print:self-stretch 
-    my-3 sm:my-0 print:my-0 mx-0 
+  <div class="border-solid border-onyx
+    border-t sm:border-t-0 print:border-t-0
+    border-l-0 sm:border-l print:border-l
+    w-24 sm:w-0 print:w-0
+    sm:self-stretch print:self-stretch
+    my-3 sm:my-0 print:my-0 mx-0
     sm:mx-2 print:mx-2"
   ></div>
 
-
-
   <Text class="text-sm font-semibold">
-    <a href="https://www.linkedin.com/in/dmitry-mikhaylov-79a01a135/" target="_blank">LinkedIn</a>
+    <a href="https://www.linkedin.com/in/dmitry-mikhaylov-79a01a135/" target="_blank" class="flex items-center">
+      <span class="mr-1">LinkedIn</span>
+      <LinkIcon/>
+    </a>
   </Text>
 
-  <div class="border-solid border-onyx 
-    border-t sm:border-t-0 print:border-t-0 
-    border-l-0 sm:border-l print:border-l 
-    w-24 sm:w-0 print:w-0 
-    sm:self-stretch print:self-stretch 
-    my-3 sm:my-0 print:my-0 mx-0 
+  <div class="border-solid border-onyx
+    border-t sm:border-t-0 print:border-t-0
+    border-l-0 sm:border-l print:border-l
+    w-24 sm:w-0 print:w-0
+    sm:self-stretch print:self-stretch
+    my-3 sm:my-0 print:my-0 mx-0
     sm:mx-2 print:mx-2"
   ></div>
 
   <Text class="text-sm font-semibold">
-    <a href="tel:+995-557-26-97-41">+995 557 26 97 41</a>
+    <a href="tel:+995-557-26-97-41" class="flex items-center">
+      <PhoneIcon class="mr-1"/>
+      +995 557 26 97 41
+    </a>
   </Text>
 </section>
